024f7c96a770df5ab09324b7476c280f4057d799
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-11-08 09:58:47 +0000

    feat: add example project

---
+++ example-go-project/flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "example-go-project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    xc = {
+      url = "github:joerdav/xc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, gitignore, xc }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ system, pkgs }: {
+        default = pkgs.buildGo121Module {
+          name = "example-go-project";
+          src = gitignore.lib.gitignoreSource ./.;
+          # Use vendored dependencies for this build.
+          vendorSha256 = null;
+        };
+
+        devShell = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              go_1_21
+              gotools
+              xc.packages.${system}.xc
+            ];
+        };
+      });
+    };
+}
