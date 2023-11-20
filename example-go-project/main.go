27b3449e026dd8dde096141c8004738082f6265d
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-11-20 09:50:14 +0000

    feat: rework example project to be a simple web server

--- example-go-project/main.go
+++ example-go-project/main.go
@@ -1,101 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
 	"os"
-
-	"github.com/charmbracelet/bubbles/spinner"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-)
-
-var (
-	// Available spinners
-	spinners = []spinner.Spinner{
-		spinner.Line,
-		spinner.Dot,
-		spinner.MiniDot,
-		spinner.Jump,
-		spinner.Pulse,
-		spinner.Points,
-		spinner.Globe,
-		spinner.Moon,
-		spinner.Monkey,
-	}
-
-	textStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("252")).Render
-	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
-	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
 )
 
 func main() {
-	m := model{}
-	m.resetSpinner()
+	addr := ":8080"
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	log.Info("Starting", slog.String("addr", addr))
 
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("could not run program:", err)
-		os.Exit(1)
-	}
+	var h Handler
+	http.ListenAndServe(addr, h)
+
+	log.Info("Exiting")
 }
 
-type model struct {
-	index   int
-	spinner spinner.Model
+type Handler struct{}
+
+func (Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World")
 }
-
-func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q", "esc":
-			return m, tea.Quit
-		case "h", "left":
-			m.index--
-			if m.index < 0 {
-				m.index = len(spinners) - 1
-			}
-			m.resetSpinner()
-			return m, m.spinner.Tick
-		case "l", "right":
-			m.index++
-			if m.index >= len(spinners) {
-				m.index = 0
-			}
-			m.resetSpinner()
-			return m, m.spinner.Tick
-		default:
-			return m, nil
-		}
-	case spinner.TickMsg:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-	default:
-		return m, nil
-	}
-}
-
-func (m *model) resetSpinner() {
-	m.spinner = spinner.New()
-	m.spinner.Style = spinnerStyle
-	m.spinner.Spinner = spinners[m.index]
-}
-
-func (m model) View() (s string) {
-	var gap string
-	switch m.index {
-	case 1:
-		gap = ""
-	default:
-		gap = " "
-	}
-
-	s += fmt.Sprintf("\n %s%s%s\n\n", m.spinner.View(), gap, textStyle("Spinning..."))
-	s += helpStyle("h/l, ←/→: change spinner • q: exit\n")
-	return
-}
-
